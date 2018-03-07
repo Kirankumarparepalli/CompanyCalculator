9df7f7f47e47290a760b86ac461e2355fadf9ba3
Kiran Parepalli <kiroooos@gmail.com>
2018-03-07 23:55:55 +1100

    Added few specs for calculator component

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -28,8 +28,76 @@ describe('CalculatorComponent', () => {
   it('should create the calculator component', async(() => {
     const fixture = TestBed.createComponent(CalculatorComponent);
     const app = fixture.debugElement.componentInstance;
-
     expect(app).toBeTruthy();
+  }));
+
+  it('should calculate the addition of 5,6 as 11', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = 5;
+    component.secondInputNumber = 6;
+    component.operation = '+';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(11);
+  }));
+
+  it('should calculate the multiplication of 9,5 as 45', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = 9;
+    component.secondInputNumber = 5;
+    component.operation = '*';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(45);
+  }));
+
+  it('should calculate the divide of 55 with 11 as 5', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = 55;
+    component.secondInputNumber = 11;
+    component.operation = '/';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(5);
+  }));
+
+  it('should calculate the sub of 31,16 as 15', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = 31;
+    component.secondInputNumber = 16;
+    component.operation = '-';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(15);
+  }));
+
+  it('should remove the values on Clear', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = 55;
+    component.secondInputNumber = 11;
+    component.operation = '/';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(5);
+    let clearResultsButton = fixture.debugElement.nativeElement.querySelector('#clearResults');
+    clearResultsButton.click();
+    expect(component.result).toEqual('');
+  }));
+
+  it('should calculate the multiplication of -5,6 as -30', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const component = fixture.debugElement.componentInstance;
+    component.firstInputNumber = -5;
+    component.secondInputNumber = 6;
+    component.operation = '*';
+    let button = fixture.debugElement.nativeElement.querySelector('#populateResults');
+    button.click();
+    expect(component.result).toEqual(-30);
   }));
 
 });
